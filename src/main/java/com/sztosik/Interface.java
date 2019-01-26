16681fafe166d381a3c4a0f8efaa038ffb03c21e
Tomek <slimak98@gmail.com>
2019-01-26 17:46:07 +0100

    trying communication with databse

--- src/main/java/com/sztosik/Interface.java
+++ src/main/java/com/sztosik/Interface.java
@@ -1,7 +1,24 @@
 package com.sztosik;
 
+import java.sql.*;
+
 public class Interface {
     public static void main(String[] args) {
-        GUI.main(null);
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://sql.amalinowska.nazwa.pl:3306/amalinowska_sztosik","amalinowska_sztosik","spioch2K19$");
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM uczniowie");
+            while (result.next()) {
+                for (int i=1; i<7; i++){
+                    System.out.print(result.getString(i) + " ");
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+//        GUI.main(null);
     }
 }
