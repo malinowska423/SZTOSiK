56c91063bd6c27e520876b2d6e2c8707fa7b9e97
Tomek <slimak98@gmail.com>
2019-01-31 02:25:29 +0100

    Admin can now see all the grades

--- src/main/java/com/sztosik/AdminControls.java
+++ src/main/java/com/sztosik/AdminControls.java
@@ -3,6 +3,7 @@ package com.sztosik;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
@@ -75,14 +76,56 @@ public class AdminControls {
 
     @FXML
     void showGrades(){
-        System.out.println("Pokazuję oceny administratora");
         setAllNotVisible();
         filtersPane.setVisible(true);
+        schedulePane.setVisible(false);
         gradesPane.setVisible(true);
+        System.out.println("Pokazuję oceny admina");
+        filtersPane.getChildren().removeAll(filtersPane.getChildren());
+        filtersPane.getChildren().add(new Label("Semestr: "));
+        ComboBox semester = new ComboBox<>(getOptions("gradesAdmin", ""));
+        semester.getSelectionModel().selectFirst();
+        filtersPane.getChildren().add(semester);
+
+        filtersPane.getChildren().add(new Label("Przedmiot: "));
+        ComboBox subject = new ComboBox<>(getOptions("subjectAdmin", semester.getSelectionModel().getSelectedItem().toString()));
+
+        filtersPane.getChildren().add(subject);
+        subject.getSelectionModel().selectFirst();
+
+        filtersPane.getChildren().add(new Label("Klasa: "));
+        ComboBox schoolClass = new ComboBox<>(getOptions("classAdmin", semester.getSelectionModel().getSelectedItem().toString() + ";"
+                + subject.getSelectionModel().getSelectedItem().toString()));
+        filtersPane.getChildren().add(schoolClass);
+        schoolClass.getSelectionModel().selectFirst();
+
+
+        subject.setOnAction(actionEvent -> {
+            if(subject.getSelectionModel().getSelectedItem() != null && semester.getSelectionModel().getSelectedItem() != null && schoolClass.getSelectionModel().getSelectedItem() != null)
+                showGradesForSemester(semester.getSelectionModel().getSelectedItem().toString(),
+                        subject.getSelectionModel().getSelectedItem().toString(), schoolClass.getSelectionModel().getSelectedItem().toString());
+        });
+
+        schoolClass.setOnAction(actionEvent -> {
+            if(schoolClass.getSelectionModel().getSelectedItem() != null && semester.getSelectionModel().getSelectedItem() != null && schoolClass.getSelectionModel().getSelectedItem() != null)
+                showGradesForSemester(semester.getSelectionModel().getSelectedItem().toString(), subject.getSelectionModel().getSelectedItem().toString(),
+                        schoolClass.getSelectionModel().getSelectedItem().toString());
+        });
+
+        semester.setOnAction(actionEvent -> {
+            subject.setItems(getOptions("subjectAdmin", semester.getSelectionModel().getSelectedItem().toString()));
+            subject.getSelectionModel().selectFirst();
+            schoolClass.setItems(getOptions("classAdmin", semester.getSelectionModel().getSelectedItem().toString() + ";" + subject.getSelectionModel().getSelectedItem().toString()));
+            schoolClass.getSelectionModel().selectFirst();
+            showGradesForSemester(semester.getSelectionModel().getSelectedItem().toString(),subject.getSelectionModel().getSelectedItem().toString(),schoolClass.getSelectionModel().getSelectedItem().toString());
+        });
+
+        showGradesForSemester(semester.getSelectionModel().getSelectedItem().toString(), subject.getSelectionModel().getSelectedItem().toString(),schoolClass.getSelectionModel().getSelectedItem().toString());
     }
 
     @FXML
     void showSchedule(){
+
         setAllNotVisible();
         filtersPane.setVisible(true);
         schedulePane.setVisible(true);
@@ -127,20 +170,41 @@ public class AdminControls {
 
     private ObservableList<String> getOptions(String filterType, String semester) {
 //        TODO: implement method where sql queries find options for filters
-        List<String> options = new ArrayList<>();
+        System.out.println(semester);
+        List <String> options = new ArrayList<>();
         switch (filterType) {
             case "semester":
                 options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT semestr FROM zajecia JOIN " +
                         "klasa_uczniowie on zajecia.id_klasy = klasa_uczniowie.id_klasy " +
-                        "WHERE id_ucznia LIKE '" + User.getInstance().getLogin() + "' ORDER BY semestr DESC;")));
+                        "JOIN kursy on zajecia.id_kursu = kursy.id_kursu " +
+                        "WHERE id_nauczyciela LIKE '" + User.getInstance().getLogin() + "' ORDER BY semestr DESC;")));
                 break;
             case "grades":
                 options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT semestr FROM oceny " +
-                        "WHERE id_ucznia LIKE '" + User.getInstance().getLogin() + "' ORDER BY semestr DESC;")));
+                        "JOIN kursy on oceny.id_kursu = kursy.id_kursu " +
+                        "WHERE id_nauczyciela LIKE '" + User.getInstance().getLogin() + "' ORDER BY semestr DESC;")));
+                break;
+            case "gradesAdmin":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT semestr FROM oceny ORDER BY semestr DESC")));
                 break;
             case "class":
                 options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT id_klasy FROM zajecia " +
                         "WHERE semestr LIKE '" + semester + "' ORDER BY id_klasy;")));
+                break;
+            case "classTeacher":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT id_klasy FROM zajecia " +
+                        "JOIN kursy on zajecia.id_kursu = kursy.id_kursu " +
+                        "JOIN przedmioty on kursy.id_przedmiotu = przedmioty.id_przedmiotu " +
+                        "WHERE semestr LIKE '" + semester.split(";")[0] + "' " +
+                        "AND id_nauczyciela LIKE '" + User.getInstance().getLogin() + "' " +
+                        "AND nazwa LIKE '" + semester.split(";")[1] + "' ORDER BY id_klasy;")));
+                break;
+            case "classAdmin":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT id_klasy FROM zajecia " +
+                        "JOIN kursy on zajecia.id_kursu = kursy.id_kursu " +
+                        "JOIN przedmioty on kursy.id_przedmiotu = przedmioty.id_przedmiotu " +
+                        "WHERE semestr LIKE '" + semester.split(";")[0] + "' " +
+                        "AND nazwa LIKE '" + semester.split(";")[1] + "' ORDER BY id_klasy;")));
                 break;
             case "teacher":
                 try {
@@ -155,9 +219,106 @@ public class AdminControls {
                 options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT id_sali FROM zajecia " +
                         "WHERE semestr LIKE '" + semester + "' ORDER BY id_sali;")));
                 break;
+            case "subject":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT nazwa FROM zajecia " +
+                        "JOIN kursy on kursy.id_kursu = zajecia.id_kursu " +
+                        "JOIN nauczyciele on kursy.id_nauczyciela = nauczyciele.pesel " +
+                        "JOIN przedmioty on kursy.id_przedmiotu = przedmioty.id_przedmiotu " +
+                        "WHERE id_nauczyciela LIKE '" + User.getInstance().getLogin() + "' " +
+                        "AND semestr LIKE '" + semester + "'" )));
+                break;
+            case "subjectAdmin":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT nazwa FROM zajecia " +
+                        "JOIN kursy on kursy.id_kursu = zajecia.id_kursu " +
+                        "JOIN nauczyciele on kursy.id_nauczyciela = nauczyciele.pesel " +
+                        "JOIN przedmioty on kursy.id_przedmiotu = przedmioty.id_przedmiotu " +
+                        "WHERE semestr LIKE '" + semester + "'" )));
+                break;
+            case "subjectClass":
+                options.addAll(FXCollections.observableArrayList(queryOptions("SELECT DISTINCT nazwa FROM zajecia " +
+                        "JOIN kursy on kursy.id_kursu = zajecia.id_kursu " +
+                        "JOIN przedmioty on kursy.id_przedmiotu = przedmioty.id_przedmiotu " +
+                        "WHERE id_klasy LIKE '" + semester.split(";")[1] + "' " +
+                        "AND semestr LIKE '" + semester.split(";")[0] + "'" )));
         }
         return FXCollections.observableArrayList(options);
     }
+
+    private void showGradesForSemester(String semester,String subject,String schoolClass) {
+
+//        TODO: run query with grades for specific semester
+        gradesPane.getChildren().clear();
+        try {
+            gradesPane.setVisible(true);
+            gradesPane.getChildren().addAll(new Label("SDFDSFDSF"));
+            System.out.println("|" + semester + "|" + User.getInstance().getLogin() + "|" + schoolClass + "|");
+            Statement statement = DatabaseConnection.connection.createStatement();
+            ResultSet result = statement.executeQuery("select oceny.id_ucznia,wartosc,waga,data,opis,imie,nazwisko,nazwa from oceny " +
+                    "join kursy k on oceny.id_kursu = k.id_kursu " +
+                    "join przedmioty p on k.id_przedmiotu = p.id_przedmiotu " +
+                    "join klasa_uczniowie ku on oceny.id_ucznia = ku.id_ucznia " +
+                    "join uczniowie u on ku.id_ucznia = u.pesel " +
+                    "and id_klasy = '" + schoolClass + "' " +
+                    "and semestr = '" + semester + "' " +
+                    "and nazwa = '" + subject + "' order by ku.id_ucznia,oceny.data;");
+            if(!result.next())
+                return;
+            String current = "xdxdxd";
+            GridPane gradesGrid = new GridPane();
+            gradesGrid.setVisible(true);
+//            gradesGrid.setPrefSize(990,665);
+            gradesGrid.setAlignment(Pos.CENTER);
+            gradesGrid.setVgap(5);
+            gradesGrid.setHgap(10);
+            gradesPane.getChildren().add(gradesGrid);
+            gradesGrid.setAlignment(Pos.CENTER);
+
+            int row = -1;
+            int column = 1;
+            System.out.println("ELOO");
+            while (result.next())
+            {
+                if(!result.getString("id_ucznia").equals(current)) {
+                    current = result.getString("id_ucznia");
+                    row++;
+                    column = 1;
+                    Label label = new Label(result.getString("imie") + " " + result.getString("nazwisko"));
+                    label.setStyle("-fx-min-height: 20;-fx-min-width: 150;-fx-alignment: center;-fx-font-size: 15");
+                    gradesGrid.add(label, 0, row);
+                }
+                Label label = new Label(result.getString("wartosc"));
+                int waga = result.getInt("waga");
+                label.setStyle("-fx-font-size: 13;-fx-min-width: 25;-fx-alignment: center;-fx-background-color: rgba(" + (255-44*waga) +"," + 50+30*waga + "," + 140+10*waga + ",1)");
+                if(result.getString("opis").equals("ocena rocz") || result.getString("opis").equals("ocena sem")){
+                    label.setStyle("-fx-font-size: 13;-fx-min-width: 25;-fx-alignment: center;-fx-background-color: orange");
+                }
+                gradesGrid.add(label, column,row);
+                column++;
+
+                Label description = new Label("waga: " + result.getString("waga") + "\ndata: " + result.getString("data") +
+                        "\nopis: " + result.getString("opis"));
+                gradesPane.getChildren().add(description);
+                description.setVisible(false);
+                description.setStyle("-fx-font-size: 12;-fx-min-width:200;-fx-min-height: 100;-fx-alignment: center;-fx-background-color: rgba(14,255,7,0.95)");
+                label.setOnMouseEntered(mouseEvent -> {
+                    description.setLayoutX(mouseEvent.getSceneX()-290);
+                    description.setLayoutY(mouseEvent.getSceneY()-55);
+                    description.setVisible(true);
+                    description.toFront();
+                });
+                label.setOnMouseExited(mouseEvent -> {
+                    description.setVisible(false);
+                });
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Pokazuję oceny ucznia w semestrze " + semester);
+
+
+    }
+
 
     private void showScheduleForClass(String semester, String schoolClass) {
 //        TODO: run query with schedule for the class in the semester
